259f94c714e7aa6d2bbefc4d63fcd425544a76a5
Sergey Starodubov <4analit@gmail.com>
2019-12-22 02:29:39 +0400

    Server run

--- socketMethods.js
+++ socketMethods.js
@@ -2,93 +2,130 @@ const _ = require('lodash');
 const yaml = require('js-yaml');
 const WebSocket = require('ws');
 
-const { getFullDepthJSON, getDescriptions } = require('./getFullDepthJSON');
-const { getAllYamls } = require('./yaml2json');
-const { argParse, getUniqueID } = require('./helpers');
+// const { getFullDepthJSON, getDescriptions } = require('./getFullDepthJSON');
+// const { getAllYamls } = require('./yaml2json');
+// const { argParse } = require('./helpers');
 
-const socketFabric = ({ args = {}, socket, envsIdIn = null, callback = () => {}, method } = {}) => {
-  if (!method) {
-    return callback;
-  } else {
-    return async ({ args, socket, envsIdIn }) => {
-      try {
-        if (!envsIdIn && method != 'createEnvs') {
-          throw { message: 'Not activate env' };
-        }
-        args = argParse(args);
-        let { envsId, envs, log } = require('./env')({ socket, envsId: envsIdIn });
-        await callback({ envsId, envs, log, args, socket, envsIdIn, method });
-      } catch (err) {
-        err.message += ` || error in '${method}'`;
-        err.socket = socket;
-        err.envId = envsIdIn;
-        socket.sendYAML({ data: { message: err.message, stack: err.stack }, type: 'error', envId: envsIdIn });
-        throw err;
-      }
-    };
-  }
-};
+// const socketFabric = ({ a = {}, socket, envsIdIn = null, callback = () => {}, method } = {}) => {
+//   if (!method) {
+//     return callback;
+//   } else {
+//     return async ({ args, socket, envsIdIn }) => {
+//       try {
+//         if (!envsIdIn && method != 'createEnvs') {
+//           throw { message: 'Not activate env' };
+//         }
+//         args = argParse(args);
+//         let { envsId, envs, log } = require('./env')({ socket, envsId: envsIdIn });
+//         await callback({ envsId, envs, log, args, socket, envsIdIn, method });
+//       } catch (err) {
+//         err.message += ` || error in '${method}'`;
+//         err.socket = socket;
+//         err.envId = envsIdIn;
+//         socket.sendYAML({ data: { message: err.message, stack: err.stack }, type: 'error', envId: envsIdIn });
+//         throw err;
+//       }
+//     };
+//   }
+// };
 
 const socketEvents = {
-
-  createEnvs: socketFabric({
-    method: 'createEnvs',
-    callback: async ({ socket, args, envsId }) => {
-      socket.sendYAML({ data: args, type: 'createEnvs', envsId });
-    },
-  }),
-
-  runEnv: socketFabric({
-    method: 'runEnv',
-    callback: async ({ socket, envs, args, envsId }) => {
-      await envs.init({ args, envsId });
-      socket.sendYAML({ data: args, type: 'runEnv', envsId });
-    },
-  }),
-
-  fetchConfigs: socketFabric({
-    method: 'fetchConfigs',
-    callback: async ({ socket, envs, args, envsId }) => {
-      socket.sendYAML({ data: args, type: 'fetchConfigs', envsId });
-    },
-  }),
-
-  fetchStruct: socketFabric({
-    method: 'fetchStruct',
-    callback: async ({ socket, envs, args, envsId }) => {
-      // await envs.init({ args, envsId });
-      const fullJSON = getFullDepthJSON({
-        envs,
-        filePath: args.testFile,
-        // testsFolder: args.testsFolder,
-        textView: true,
-      });
-      socket.sendYAML({ data: fullJSON, type: 'fullJSON', envsId });
-      const fullDescriptions = getDescriptions();
-      socket.sendYAML({ data: fullDescriptions, type: 'fullDescriptions', envsId });
-    },
-  }),
-
-  fetchAvailableTests: async ({ args = {}, socket, envsIdIn = null } = {}) => {
-    try {
-      if (!envsIdIn) {
-        throw { message: 'Not activate env' };
-      }
-      args = argParse(args);
-      let { envsId, envs, log } = require('./env')({ socket, envsId: envsIdIn });
-      await envs.init({ args, envsId });
-
-      const testsFolder = _.get(envs, ['args', 'testsFolder'], '.');
-      const allYamls = await getAllYamls({ testsFolder, envsId });
-      socket.sendYAML({ data: allYamls, type: 'allYamls', envsId });
-    } catch (err) {
-      err.message += ` || error in 'fetchAvailableTests'`;
-      err.socket = socket;
-      err.envId = envId;
-      throw err;
-    }
-  },
+  // createEnvs: socketFabric({
+  //   method: 'createEnvs',
+  //   callback: async ({ socket, args, envsId }) => {
+  //     socket.sendYAML({ data: args, type: 'createEnvs', envsId });
+  //   },
+  // }),
+  // runEnv: socketFabric({
+  //   method: 'runEnv',
+  //   callback: async ({ socket, envs, args, envsId }) => {
+  //     await envs.init({ args, envsId });
+  //     socket.sendYAML({ data: args, type: 'runEnv', envsId });
+  //   },
+  // }),
+  // fetchConfigs: socketFabric({
+  //   method: 'fetchConfigs',
+  //   callback: async ({ socket, envs, args, envsId }) => {
+  //     socket.sendYAML({ data: args, type: 'fetchConfigs', envsId });
+  //   },
+  // }),
+  // fetchStruct: socketFabric({
+  //   method: 'fetchStruct',
+  //   callback: async ({ socket, envs, args, envsId }) => {
+  //     // await envs.init({ args, envsId });
+  //     const fullJSON = getFullDepthJSON({
+  //       envs,
+  //       filePath: args.testFile,
+  //       // testsFolder: args.testsFolder,
+  //       textView: true,
+  //     });
+  //     socket.sendYAML({ data: fullJSON, type: 'fullJSON', envsId });
+  //     const fullDescriptions = getDescriptions();
+  //     socket.sendYAML({ data: fullDescriptions, type: 'fullDescriptions', envsId });
+  //   },
+  // }),
+  // fetchAvailableTests: async ({ args = {}, socket, envsIdIn = null } = {}) => {
+  //   try {
+  //     if (!envsIdIn) {
+  //       throw { message: 'Not activate env' };
+  //     }
+  //     args = argParse(args);
+  //     let { envsId, envs, log } = require('./env')({ socket, envsId: envsIdIn });
+  //     await envs.init({ args, envsId });
+  //     const testsFolder = _.get(envs, ['args', 'testsFolder'], '.');
+  //     const allYamls = await getAllYamls({ testsFolder, envsId });
+  //     socket.sendYAML({ data: allYamls, type: 'allYamls', envsId });
+  //   } catch (err) {
+  //     err.message += ` || error in 'fetchAvailableTests'`;
+  //     err.socket = socket;
+  //     err.envId = envId;
+  //     throw err;
+  //   }
+  // },
 };
+
+// const fetchStruct = async (args = {}, socket) => {
+//   try {
+//     args = new Arguments().init(args);
+//     socket.sendYAML({ data: args, type: 'init_args' });
+//     let { envsId, envs } = require('./env')({ socket });
+//     await envs.init();
+
+//     await new TestsContent({
+//       rootFolder: args.PPD_ROOT,
+//       additionalFolders: args.PPD_ROOT_ADDITIONAL,
+//       ignorePaths: args.PPD_ROOT_IGNORE,
+//     }).getAllData();
+//     const { fullJSON, textDescription } = getFullDepthJSON({
+//       testName: args.testFile,
+//     });
+//     socket.sendYAML({ data: fullJSON, type: 'fullJSON', envsId });
+//     socket.sendYAML({ data: textDescription, type: 'fullDescriptions', envsId });
+//   } catch (err) {
+//     err.message += ` || error in 'fetchStruct'`;
+//     err.socket = socket;
+//     throw err;
+//   }
+// };
+
+// const fetchAvailableTests = async (args = {}, socket) => {
+//   try {
+//     args = new Arguments().init(args);
+//     socket.sendYAML({ data: args, type: 'init_args' });
+//     let { envsId, envs } = require('./env')({ socket });
+//     await envs.init();
+//     const allYamls = await new TestsContent({
+//       rootFolder: args.PPD_ROOT,
+//       additionalFolders: args.PPD_ROOT_ADDITIONAL,
+//       ignorePaths: args.PPD_ROOT_IGNORE,
+//     }).getAllData();
+//     socket.sendYAML({ data: allYamls, type: 'allYamls', envsId });
+//   } catch (err) {
+//     err.message += ` || error in 'fetchAvailableTests'`;
+//     err.socket = socket;
+//     throw err;
+//   }
+// };
 
 const socketMethods = async ({ args = {}, socket, envsId, method } = {}) => {
   if (socketEvents[method]) {
@@ -96,6 +133,15 @@ const socketMethods = async ({ args = {}, socket, envsId, method } = {}) => {
   } else {
     throw { message: `Can't find method: ${method} in socket server` };
   }
+};
+
+const getUniqueID = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4();
 };
 
 const createSocketServer = () => {
@@ -124,6 +170,8 @@ const createSocketServer = () => {
     ws.onerror = () => {};
     ws.onopen = () => {};
   });
+
+  console.log('Server run on port 3001');
 
   return wss;
 };
